e73bf1faa9021b023d6b680fbfffde40bbaf07ae
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-16 18:12:12 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          has_many :messages
+
+    validates :name, presence: true
 end
