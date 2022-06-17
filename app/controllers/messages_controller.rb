520ce048705a12b5aa3dc0e369f04ff5d7a426e4
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-17 11:01:20 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,19 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find (params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    @message.save
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
   end
-
   private
 
   def message_params
